22795656ea3f2792e1f45d6d7084f78426749138
Yevgeniy5056 <149609362+Yevgeniy5056@users.noreply.github.com>
2024-05-14 10:10:52 -0400

    String connection to db, User and Role tables created

--- src/main/java/com/cydeo/entity/Role.java
+++ src/main/java/com/cydeo/entity/Role.java
@@ -1,15 +1,19 @@
 package com.cydeo.entity;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+import javax.persistence.Entity;
+
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Role {
+@Entity(name = "roles")
+public class Role extends BaseEntity {
 
-    private Long id;
     private String description;
 
 }
--- src/main/java/com/cydeo/entity/User.java
+++ src/main/java/com/cydeo/entity/User.java
@@ -1,13 +1,19 @@
 package com.cydeo.entity;
 
 import com.cydeo.enums.Gender;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import java.time.LocalDateTime;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.ManyToOne;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
+@Entity(name = "users")
 public class User extends BaseEntity {
 
     private String firstName;
@@ -16,19 +22,9 @@ public class User extends BaseEntity {
     private String passWord;
     private boolean enabled;
     private String phone;
+    @ManyToOne
     private Role role;
+    @Enumerated(EnumType.STRING)
     private Gender gender;
-
-    public User(Long id, LocalDateTime insertDateTime, Long insertUserId, LocalDateTime lastUpdateDateTime, Long lastUpdateUserId, String firstName, String lastName, String userName, String passWord, boolean enabled, String phone, Role role, Gender gender) {
-        super(id, insertDateTime, insertUserId, lastUpdateDateTime, lastUpdateUserId);
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.userName = userName;
-        this.passWord = passWord;
-        this.enabled = enabled;
-        this.phone = phone;
-        this.role = role;
-        this.gender = gender;
-    }
 
 }
