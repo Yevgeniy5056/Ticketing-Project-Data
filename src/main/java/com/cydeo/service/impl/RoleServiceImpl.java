45b2b6181674d3b832367f6f5f3ad8a13e1d6341
Yevgeniy5056 <149609362+Yevgeniy5056@users.noreply.github.com>
2024-05-14 12:15:41 -0400

    RoleServiceImpl RoleMapper injected, getAllRoles() completed

--- src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
@@ -1,17 +1,27 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.RoleDTO;
+import com.cydeo.mapper.RoleMapper;
+import com.cydeo.repository.RoleRepository;
 import com.cydeo.service.RoleService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor
 public class RoleServiceImpl implements RoleService {
+
+    private final RoleRepository roleRepository;
+    private final RoleMapper roleMapper;
 
     @Override
     public List<RoleDTO> getAllRoles() {
-        return List.of();
+
+        return roleRepository.findAll().stream().map(roleMapper::convertToDto).collect(Collectors.toList());
+
     }
 
     @Override
