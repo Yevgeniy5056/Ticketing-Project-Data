da05bcd511593272c268644e3dca277eb15c04bc
Yevgeniy5056 <149609362+Yevgeniy5056@users.noreply.github.com>
2024-05-15 10:44:16 -0400

    UserServiceImpl delete completed, UserRepository deleteByUserName @Transactional added

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -43,6 +43,9 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void deleteByUserName(String username) {
+
+        userRepository.deleteByUserName(username);
+
     }
 
     @Override
