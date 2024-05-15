3b78f18bd71b7a92ffb9096d531a5c228014b009
Yevgeniy5056 <149609362+Yevgeniy5056@users.noreply.github.com>
2024-05-15 08:18:14 -0400

    UserMapper created , UserDtoConverter method name changed , UserServiceImpl getAllUsers completed

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -1,16 +1,29 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.UserDTO;
+import com.cydeo.mapper.UserMapper;
+import com.cydeo.repository.UserRepository;
 import com.cydeo.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
     @Override
     public List<UserDTO> getAllUsers() {
-        return List.of();
+
+        return userRepository.findAll(Sort.by("firstName")).stream()
+                .map(userMapper::convertToDto).collect(Collectors.toList());
+
     }
 
     @Override
