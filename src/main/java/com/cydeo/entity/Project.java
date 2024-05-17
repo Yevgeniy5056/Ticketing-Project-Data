b144b80314816337eab2d3fdde4e67a352801436
Yevgeniy5056 <149609362+Yevgeniy5056@users.noreply.github.com>
2024-05-17 10:35:42 -0400

    Project entity created

---
+++ src/main/java/com/cydeo/entity/Project.java
@@ -0,0 +1,34 @@
+package com.cydeo.entity;
+
+import com.cydeo.enums.Status;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity(name = "projects")
+@Where(clause = "is_deleted=false")
+public class Project extends BaseEntity {
+
+    private String projectCode;
+    private String projectName;
+    @Column(columnDefinition = "DATE")
+    private LocalDate startDate;
+    @Column(columnDefinition = "DATE")
+    private LocalDate endDate;
+    @Enumerated(EnumType.STRING)
+    private Status projectStatus;
+    @Column(columnDefinition = "text")
+    private String projectDetail;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "manager_id")
+    private User assignedManager;
+
+
+}
