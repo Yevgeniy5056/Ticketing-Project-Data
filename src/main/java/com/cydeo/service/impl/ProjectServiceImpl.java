f3f24c5fd2240253760d1d10642afc01d63a6a51
Yevgeniy5056 <149609362+Yevgeniy5056@users.noreply.github.com>
2024-05-17 12:42:15 -0400

    ProjectServiceImpl getAllProjects done

--- src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -1,13 +1,25 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.ProjectDTO;
+import com.cydeo.mapper.ProjectMapper;
+import com.cydeo.repository.ProjectRepository;
 import com.cydeo.service.ProjectService;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ProjectServiceImpl implements ProjectService {
+
+    private final ProjectRepository projectRepository;
+    private final ProjectMapper projectMapper;
+
+    public ProjectServiceImpl(ProjectRepository projectRepository, ProjectMapper projectMapper) {
+        this.projectRepository = projectRepository;
+        this.projectMapper = projectMapper;
+    }
 
     @Override
     public ProjectDTO getByProjectCode(String code) {
@@ -16,7 +28,10 @@ public class ProjectServiceImpl implements ProjectService {
 
     @Override
     public List<ProjectDTO> getAllProjects() {
-        return List.of();
+
+        return projectRepository.findAll(Sort.by("projectCode")).stream()
+                .map(projectMapper::convertToDto).collect(Collectors.toList());
+
     }
 
     @Override
