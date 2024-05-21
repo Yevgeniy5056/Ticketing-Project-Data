494add2b475d8fe24b8ed27f26dae78a801803ce
Yevgeniy5056 <yevgengeodakyan@gmail.com>
2024-05-21 16:26:29 -0400

    TaskServiceImpl save() and getAllTasks() done

--- src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -1,21 +1,40 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.TaskDTO;
+import com.cydeo.enums.Status;
+import com.cydeo.mapper.TaskMapper;
+import com.cydeo.repository.TaskRepository;
 import com.cydeo.service.TaskService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor
 public class TaskServiceImpl implements TaskService {
+
+    private final TaskRepository taskRepository;
+    private final TaskMapper taskMapper;
 
     @Override
     public List<TaskDTO> getAllTasks() {
-        return List.of();
+
+        return taskRepository.findAll().stream()
+                .map(taskMapper::convertToDto).collect(Collectors.toList());
+
     }
 
     @Override
     public void save(TaskDTO task) {
+
+        task.setTaskStatus(Status.OPEN);
+
+        task.setAssignedDate(LocalDate.now());
+
+        taskRepository.save(taskMapper.convertToEntity(task));
 
     }
 
