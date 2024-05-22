ee7bcdf2df8c64a1753708e6ee88afe0e6e5ade5
Yevgeniy5056 <yevgengeodakyan@gmail.com>
2024-05-21 20:01:57 -0400

    ProjectServiceImpl getAllProjectDetails() implemented

--- src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -1,11 +1,15 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.ProjectDTO;
+import com.cydeo.dto.UserDTO;
 import com.cydeo.entity.Project;
+import com.cydeo.entity.User;
 import com.cydeo.enums.Status;
 import com.cydeo.mapper.ProjectMapper;
+import com.cydeo.mapper.UserMapper;
 import com.cydeo.repository.ProjectRepository;
 import com.cydeo.service.ProjectService;
+import com.cydeo.service.UserService;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
@@ -17,10 +21,14 @@ public class ProjectServiceImpl implements ProjectService {
 
     private final ProjectRepository projectRepository;
     private final ProjectMapper projectMapper;
+    private final UserService userService;
+    private final UserMapper userMapper;
 
-    public ProjectServiceImpl(ProjectRepository projectRepository, ProjectMapper projectMapper) {
+    public ProjectServiceImpl(ProjectRepository projectRepository, ProjectMapper projectMapper, UserService userService, UserMapper userMapper) {
         this.projectRepository = projectRepository;
         this.projectMapper = projectMapper;
+        this.userService = userService;
+        this.userMapper = userMapper;
     }
 
     @Override
@@ -79,5 +87,31 @@ public class ProjectServiceImpl implements ProjectService {
         project.setProjectStatus(Status.COMPLETE);
 
         projectRepository.save(project);
+
     }
+
+    @Override
+    public List<ProjectDTO> getAllProjectDetails() {
+
+        UserDTO currentUserDTO = userService.findByUserName("harold@manager.com");
+
+        User user = userMapper.convertToEntity(currentUserDTO);
+
+        List<Project> projects = projectRepository.findAllByAssignedManager(user);
+
+
+        return projects.stream().map(project -> {
+
+            ProjectDTO projectDTO = projectMapper.convertToDto(project);
+
+            projectDTO.setUnfinishedTaskCounts(3);
+            projectDTO.setCompleteTaskCounts(5);
+
+            return projectDTO;
+
+        }).collect(Collectors.toList());
+
+    }
+
+
 }
